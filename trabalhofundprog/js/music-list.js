89106405710df5321f541ff187bc9fc728a5d0c6
~leticiadutra <101335613+leticiadutra22-23@users.noreply.github.com>
2022-06-11 13:36:51 -0300

    version 1.0 - web music player
    Music Player in JavaScript code from: https://www.youtube.com/watch?v=1-CvPn4AbT4
    
    'Create Custom Music Player in HTML CSS & JavaScript' - CodingNepal
    
    Thank you!

---
+++ trabalhofundprog/js/music-list.js
@@ -0,0 +1,56 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Boys Don't Cry",
+    artist: "The Cure",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Little Dark Age",
+    artist: "MGMT",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Psycho Killer (2005 Remaster)",
+    artist: "Talking Heads",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Rhinestone Eyes",
+    artist: "Gorillaz",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Running Up That Hill (A Deal With God) (2018 Remaster)",
+    artist: "Kate Bush",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "The Final Countdown",
+    artist: "Europe",
+    img: "music-6",
+    src: "music-6"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
