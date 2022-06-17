accc72d66d2890cd4f1c978fdfd5b430a92a5fb5
~leticiadutra <101335613+leticiadutra22-23@users.noreply.github.com>
2022-06-17 17:00:38 -0300

    complete

--- trabalhofundprog/js/music-list.js
+++ trabalhofundprog/js/music-list.js
@@ -1,14 +1,3 @@
-// To add more song, just copy the following code and paste inside the array
-
-//   {
-//     name: "Here is the music name",
-//     artist: "Here is the artist name",
-//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
-//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
-//   }
-
-//paste it inside the array as more as you want music then you don't need to do any other thing
-
 let allMusic = [
   {
     name: "Boys Don't Cry",
@@ -46,11 +35,46 @@ let allMusic = [
     img: "music-6",
     src: "music-6"
   },
-  // like this paste it and remember to give comma after ending of this bracket }
-  // {
-  //   name: "Here is the music name",
-  //   artist: "Here is the artist name",
-  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
-  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
-  // }
+  {
+    name: "One Kiss",
+    artist: "Calvin Harris, Dua Lipa",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Hayloft II",
+    artist: "Mother Mother",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "505",
+    artist: "Arctic Monkeys",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "O Descobridor Dos Sete Mares",
+    artist: "Tim Maia",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Coringa",
+    artist: "Jão",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "Lost Boy",
+    artist: "Ruth B.",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "Dear Parents",
+    artist: "Tate McRae",
+    img: "music-13",
+    src: "music-13"
+  },
 ];
